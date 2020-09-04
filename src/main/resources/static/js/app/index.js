cc24d86633bf3371724fe7b9dd0e2092820e10bd
Sun <syseoz@naver.com>
2020-09-04 19:57:32 +0900

    mustache 등록 화면

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,30 @@
+var index = {
+    init :function () {
+        var _this = this;
+        $('#btn-save').on('click',function () {
+            _this.save();
+        });
+
+    },
+    save:function () {
+        var data={
+            title:$('#title').val(),
+            content:$('#content').val(),
+            author:$('#author').val()
+        };
+
+        $.ajax({
+            type:'POST',
+            url:'/api/v1/posts',
+            dataType:'json',
+            contentType:'application/json; charset=utf-8',
+            data:JSON.stringify(data)
+        }).done(function () {
+            alert('등록 완료');
+            window.location.href='/';
+        }).fail(function (error) {
+            alert(error);
+        })
+    }
+}
+index.init();
